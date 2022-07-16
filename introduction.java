0d352f44c39307ae20db431d9b3aaa4fb6f7d7a0
bogeshwararao <bogeshwararao12042004@gmail.com>
2022-07-16 10:50:55 +0530

    commit

--- introduction.java
+++ introduction.java
@@ -1,4 +1,5 @@
-class HelloWorld {
+//The below proggram is to show the out of any given statement. 
+public class introduction {
     public static void main(String[] args) {
         System.out.println("Hello, world! hihihihi"); 
     }
