7c00efc685a13c9845c361eb75b423338bc2e634
bogeshwararao <bogeshwararao12042004@gmail.com>
2022-07-17 17:30:53 +0530

    commit

--- introduction/primitive.java
+++ introduction/primitive.java
@@ -1,3 +1,6 @@
+//Here the primitive means that the particular word that can be breakable it two is know as primitive.
+//Example: 6 is an integer that which can not be breake it to sub particles.
+//That suppose "Bogesh" it can be divided in to 'b','o','g','e','s','h' it can be divided...:) 
 package introduction;
 
 public class primitive {
