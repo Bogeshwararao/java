695fac2e107792fc333bc9ee6882081902dd8962
bogeshwararao <bogeshwararao12042004@gmail.com>
2022-07-18 11:35:51 +0530

    coomit

---
+++ introduction/conditions/conditions.java
@@ -0,0 +1,15 @@
+package introduction.conditions;
+
+public class conditions {
+    public static void main(String[] args) {
+        int a= 100;
+        int b= 300;
+        if (a>b){
+            System.out.println(a+" is grater than the "+ b);
+        }else {
+            System.out.println(b+" is grater than the "+ a);
+        }
+        
+    }
+           
+}
