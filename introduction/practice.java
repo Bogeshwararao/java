1953cc8654afdde40eadedee0b6c133ce802438e
bogeshwararao <bogeshwararao12042004@gmail.com>
2022-07-20 18:36:54 +0530

    commit

---
+++ introduction/practice.java
@@ -0,0 +1,28 @@
+package introduction;
+import java.util.Scanner;
+public class practice {
+            public static void main(String[] args) {
+                Scanner in = new Scanner(System.in);
+                String x=in.next();
+                 
+                System.out.println(x.substring(0, 1).toUpperCase()+ x.substring(1));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }    
+}
