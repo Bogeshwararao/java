370ffd39bf4289b4c17b6289920352651b91225c
bogeshwararao <bogeshwararao12042004@gmail.com>
2022-07-20 18:44:42 +0530

    commit

---
+++ HackerRank/SecondProblem.java
@@ -0,0 +1,14 @@
+package HackerRank;
+import java.util.Scanner;
+public class SecondProblem {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+String myString = scanner.next();
+int myInt = scanner.nextInt();
+scanner.close();
+
+System.out.println("myString is: " + myString);
+System.out.println("myInt is: " + myInt);
+    }
+    
+}
