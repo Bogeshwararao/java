9576475bea24008f42d162516631ba4237e9d4f0
bogeshwararao <bogeshwararao12042004@gmail.com>
2022-07-20 18:42:32 +0530

    commit

--- HackerRank/Firstproblem.java
+++ HackerRank/Firstproblem.java
@@ -1,5 +1,10 @@
+//  ***********WELCOME TO JAVA**************//
+
 package HackerRank;
 
 public class Firstproblem {
-    
+       public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+       }
 }
