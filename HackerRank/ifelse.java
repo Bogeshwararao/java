146aee3ec4ff6f2a76d4004ea6d482a0269dcc32
bogeshwararao <bogeshwararao12042004@gmail.com>
2022-07-23 16:13:16 +0530

    ifelse completed

--- HackerRank/ifelse.java
+++ HackerRank/ifelse.java
@@ -1,7 +1,28 @@
 package HackerRank;
-
+import java.util.Scanner;
 public class ifelse {
             public static void main(String[] args) {
-                
+                      Scanner in=new Scanner(System.in);
+                      int N= in.nextInt();
+                      if(N%2 == 0)
+                      {
+                          if(N>=2 && N<=5)
+                          {
+                              System.out.println("Not Weird");
+                          }
+                          else if(N>=6 && N<=20)
+                          {
+                              System.out.println("Weird");
+                          }
+                          else
+                          {
+                              System.out.println("Not Weird");
+                          } 
+                      }
+                      else
+                      {
+                          System.out.println("Weird");
+                      }
+
             }    
 }
