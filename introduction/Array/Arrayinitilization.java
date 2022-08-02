e6e8bb4f0d806fbac53dd2cb1c568c3570d0b097
bogeshwararao <bogeshwararao12042004@gmail.com>
2022-08-02 10:17:27 +0530

    array updated

--- introduction/Array/Arrayinitilization.java
+++ introduction/Array/Arrayinitilization.java
@@ -1,5 +1,17 @@
 package introduction.Array;
 
 public class Arrayinitilization {
-    
+      public static void main(String[] args) {
+            // Datatype[] varible; 
+                   /* OR */
+            //Datatype[] varible = {"name", "class","section"  WHAT EVER   };
+            String[] Names = {"BOGESH", "ABISH"};
+            int[]    NUMBERS= {1,2,3,4,5};
+
+            /*********************INDEX NUMBER***************/
+            //THE INDEX NUMBER ALWAYS START WITH THE ZERO(0)//
+               String[] myName= {"BOGESH"};
+               System.out.println(myName[0]);
+
+      }
 }
