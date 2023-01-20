25ad1894bce032c338f340ae913b256c5936a21d
bogeshwararao <bogeshwararao12042004@gmail.com>
2023-01-20 23:54:31 +0630

    commit

--- introduction/Linkelist/LL.java
+++ introduction/Linkelist/LL.java
@@ -105,5 +105,19 @@ public class LL {
         return val;
     }
       
-   
+    public static void main(String[] args) {
+        LL list = new LL();
+        list.insertFirst(3);
+        list.insertFirst(2);
+        list.insertFirst(8);
+        list.insertFirst(17);
+        
+        list.insertLast(23);
+        list.Insert(12,3);
+        list.display();
+        //list.deleteFirst();
+      //System.out.println(list.deleteLast()); 
+       list.delete(2);
+        list.display();
+    }
 }
--- introduction/Linkelist/Main.java
+++ introduction/Linkelist/Main.java
@@ -1,18 +1,19 @@
-package introduction.Linkelist;
+// package introduction.Linkelist;
 
-public class Main {
-    public static void main(String[] args) {
-        LL list = new LL();
-        list.insertFirst(3);
-        list.insertFirst(2);
-        list.insertFirst(8);
-        list.insertFirst(17);
+// public class Main {
+//     public static void main(String[] args) {
+//         LL list = new LL();
+//         list.insertFirst(3);
+//         list.insertFirst(2);
+//         list.insertFirst(8);
+//         list.insertFirst(17);
         
-        list.insertLast(23);
-        list.Insert(12,3);
-        list.display();
-        //list.deleteFirst();
-       System.out.println(list.deleteLast()); 
-        list.display();
-    }
-}
+//         list.insertLast(23);
+//         list.Insert(12,3);
+//         list.display();
+//         //list.deleteFirst();
+//       //System.out.println(list.deleteLast()); 
+//        list.delete(2);
+//         list.display();
+//     }
+// }
