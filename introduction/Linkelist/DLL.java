6c970c118ca6cc242df7561d4a46e808302a31db
bogeshwararao <bogeshwararao12042004@gmail.com>
2023-01-23 19:01:46 +0630

    commit

--- introduction/Linkelist/DLL.java
+++ introduction/Linkelist/DLL.java
@@ -2,5 +2,12 @@ package introduction.Linkelist;
 
 public class DLL {
     private Node head;
-    
+    public Node(int value) {
+        this.value = value;
+    }
+
+    public Node(int value, Node next) {
+        this.value = value;
+        this.next = next;
+    }
 }
